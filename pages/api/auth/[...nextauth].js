59be557edb76ba0726a877f0714239dac59b99bb
onrylcn <onuryalcintr@gmail.com>
2023-06-09 20:25:27 +0300

    Github Login..

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,7 +1,10 @@
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
+import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
+import clientPromise from "@/lib/mongo";
+
 export default NextAuth({
-  // Configure one or more authentication providers
+  adapter: MongoDBAdapter(clientPromise),
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_ID,
